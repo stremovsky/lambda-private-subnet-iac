d711f2d87ca79f9b42c71fd0da9707e446e955c5
yuli <stremovsky@gmail.com>
2024-08-08 11:54:47 +0300

    small optimization

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/prod/getip"
+  value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/getip"
 }
